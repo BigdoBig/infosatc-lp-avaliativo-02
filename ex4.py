c0589172c493062747a8a0f14ccb7e8264d7ceb3
Augusto Frelo Venturini <88850459+BigdoBig@users.noreply.github.com>
2021-09-09 15:50:48 -0300

    Create ex4.py

---
+++ ex4.py
@@ -0,0 +1,8 @@
+lista_frutas = ["maça" , "banana" , "abacaxi" , "quiwi" , "acerola"]
+
+print("1 Lista criada = Lista de frutas")
+print(lista_frutas)
+
+print("Lista copiada")
+lista_frutasnova = lista_frutas.copy()
+print(lista_frutasnova)
