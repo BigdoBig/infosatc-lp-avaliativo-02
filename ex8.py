924f3a623b173cd45d9529a8b0ec512cf5817f28
Augusto Frelo Venturini <88850459+BigdoBig@users.noreply.github.com>
2021-09-09 16:27:15 -0300

    Create ex8.py

---
+++ ex8.py
@@ -0,0 +1,10 @@
+lista_pc = ["gabinete cooler master" , "AORUS 550" , "GEFORCE RTX 2080Ti" , "Ryzen 5 5900x" , "Memória RAM" , "SSD nvme 1 gb", "Fonte 650W" , "Cooler"]
+print("Passou-se do valor acessível! ")
+print(lista_pc)
+
+
+del lista_pc[0]
+print(lista_pc)
+
+del lista_pc[1]
+print(lista_pc)
