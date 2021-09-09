1db7e3950742ee2d2cde682ef7ecfccb070cba63
Augusto Frelo Venturini <88850459+BigdoBig@users.noreply.github.com>
2021-09-09 15:55:19 -0300

    Create ex5.py

---
+++ ex5.py
@@ -0,0 +1,10 @@
+lista_compras = ["frutas" , "papel toalha" , "azeite" , "cerveja"]
+
+if "cerveja" in lista_compras:
+    print("Sim este item está presente na sua lista de compras! ")
+else:
+ print("Este item não esta presente na lista")
+
+
+
+     
