16c059e587538ff6cfe86a420c01ec4445a38790
Augusto Frelo Venturini <88850459+BigdoBig@users.noreply.github.com>
2021-09-09 16:36:05 -0300

    Create ex9.py

---
+++ ex9.py
@@ -0,0 +1,15 @@
+nume1 = int(input("Digite o 1 número! "))
+nume2 = int(input("Digite o 2 número! "))
+nume3 = int(input("Digite o 3 número! "))
+nume4 = int(input("Digite o 4 número! "))
+nume5 = int(input("Digite o 5 número! "))
+nume6 = int(input("Digite o 6 número! "))
+nume7 = int(input("Digite o 7 número! "))
+nume8= int(input("Digite o 8 número! "))
+nume9= int(input("Digite o 9 número! "))
+nume10 = int(input("Digite o 10 número! "))
+
+lista_numeros = [nume1,nume2,nume3,nume4,nume5,nume6,nume7,nume8,nume9,nume10]
+print(lista_numeros)
+
+print("A sua lista de números orgazinada é : ", sorted(lista_numeros))
